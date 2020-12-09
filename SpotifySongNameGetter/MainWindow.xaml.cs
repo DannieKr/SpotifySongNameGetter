d6b1ea99bb3cd1479ae91f778a78c78795d6d82d
danniekr <dannie.kro@gmail.com>
2020-12-09 16:43:08 +0100

    initial commit

---
+++ SpotifySongNameGetter/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpotifySongNameGetter
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCheck_Click(object sender, RoutedEventArgs e)
+        {
+            txtBox.Text = getSpotifySongName();
+        }
+
+        private string getSpotifySongName()
+        {
+            var proc = Process.GetProcessesByName("Spotify").FirstOrDefault(p => !string.IsNullOrWhiteSpace(p.MainWindowTitle));
+            return proc.MainWindowTitle;
+        }
+
+        private void btnCheckAndClipboard_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(getSpotifySongName());
+            txtBox.Text = getSpotifySongName();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
